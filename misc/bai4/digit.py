a18fde7526a2096f81f20aa7fdba2230499091e9
kimstars <buikika9@gmail.com>
2023-12-22 16:00:55 +0700

    update bai 4

--- misc/bai4/digit.py
+++ misc/bai4/digit.py
@@ -4,11 +4,22 @@ context.log_level = 'debug'
 # nc 103.162.14.116 14003
 r = remote('103.162.14.116', 14004)
 
-
-stringnum = ""
-
-for i in range(0,99999):
-    stringnum += str(i) 
+def find_ith_digit(i):
+    # Bước 1: Khởi tạo các biến
+    start = 1  # Giá trị bắt đầu của khoảng số
+    digits = 1  # Số lượng chữ số
+    while True:
+        # Bước 2: Xác định độ dài của các khoảng số có số lượng chữ số tăng dần
+        count = 9 * start * digits
+        if i <= count:
+            # Bước 3: Xác định số cụ thể trong khoảng số
+            number = start + (i - 1) // digits
+            # Bước 4: Xác định chữ số tại vị trí i trong số đó
+            return str(number)[(i - 1) % digits]
+        i -= count
+        start *= 10
+        digits += 1
+        
     
 print ("nhan duoc 1=> ", r.recv())
 
@@ -17,7 +28,10 @@ while(1):
 
     print(n)
 
-    r.send(stringnum[n].encode())
+    r.send(find_ith_digit(n).encode())
 
     print ("nhan duoc 1=> ", r.recv())
     print ("nhan duoc 1=> ", r.recv())
+
+
+# KCSC{dO_yOu_knOw_prOgrAmmIng_Is_vErY_ImpOrtAnt?}
--- misc/bai4/test.py
+++ misc/bai4/test.py
@@ -1,7 +1,23 @@
+def find_ith_digit(i):
+    # Bước 1: Khởi tạo các biến
+    start = 1  # Giá trị bắt đầu của khoảng số
+    digits = 1  # Số lượng chữ số
+    while True:
+        # Bước 2: Xác định độ dài của các khoảng số có số lượng chữ số tăng dần
+        count = 9 * start * digits
+        if i <= count:
+            # Bước 3: Xác định số cụ thể trong khoảng số
+            number = start + (i - 1) // digits
+            # Bước 4: Xác định chữ số tại vị trí i trong số đó
+            return int(str(number)[(i - 1) % digits])
+        i -= count
+        start *= 10
+        digits += 1
+        
+# Ví dụ sử dụng:
+i = 2660284
+result = find_ith_digit(i)
+print(f"Ký tự thứ {i} là: {result}")
 
-stringnum = ""
 
-for i in range(0,100):
-    stringnum += str(i) 
-    
-print(stringnum[5]) 
+# khó quá nhờ chatgpt
